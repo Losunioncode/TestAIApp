647edc64128fc1f516ccd2495d1e996ba6117426
Losunioncode <klim.lipkin45@gmail.com>
2024-02-12 12:15:50 +0100

    alpha v1.2.1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { withNextVideo } = require('next-video/process')
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,4 +25,4 @@ const nextConfig = {
     }
   }
   
-module.exports = withNextVideo(nextConfig)
+module.exports = nextConfig
